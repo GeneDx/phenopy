6eee2ab7f49c15fdaf7afadf11d114982a17ff9f
KevinA <karvai@genedx.com>
2019-01-23 11:37:37 -0500

    add generic scoring of both input files

--- phenosim/__main__.py
+++ phenosim/__main__.py
@@ -84,7 +84,74 @@ def score_case_to_genes(case_hpo_file, obo_file=None, pheno2genes_file=None, thr
         p.starmap(scorer.score_pairs, [(genes_to_terms, [('case', gene) for gene in genes_to_terms], lock, i, threads) for i in range(threads)])
 
 
-def score_all(records_file, obo_file=None, pheno2genes_file=None, threads=1):
+def score_case_to_cases(case_hpo_file, records_file, obo_file=None, pheno2genes_file=None, threads=1):
+    """
+    Scores a case HPO terms against many cases with HPO terms (in the same format as `tests/data/test.score-all.txt`).
+
+    :param case_hpo_file: File with case HPO terms, one per line.
+    :param records_file: One record per line, tab delimited. First column record unique identifier, second column
+        pipe separated list of HPO identifier (HP:0000001).
+    :param obo_file: OBO file from https://hpo.jax.org/app/download/ontology.
+    :param pheno2genes_file: Phenotypes to genes from https://hpo.jax.org/app/download/annotation.
+    """
+    if obo_file is None:
+        try:
+            obo_file = config.get('hpo', 'obo_file')
+        except (NoSectionError, NoOptionError):
+            logger.critical('No HPO OBO file provided and no "hpo:obo_file" found in the configuration file.')
+            exit(1)
+
+    if pheno2genes_file is None:
+        try:
+            pheno2genes_file = config.get('hpo', 'pheno2genes_file')
+        except (NoSectionError, NoOptionError):
+            logger.critical(
+                'No HPO pheno2genes_file file provided and no "hpo:pheno2genes_file" found in the configuration file.'
+            )
+            exit(1)
+
+    try:
+        with open(case_hpo_file, 'r') as case_fh:
+            case_hpo = case_fh.read().splitlines()
+    except (FileNotFoundError, PermissionError) as e:
+        logger.critical(e)
+        exit(1)
+
+    # load phenotypes to genes associations
+    terms_to_genes, _, annotations_count = load_p2g(pheno2genes_file, logger=logger)
+
+    # load hpo network
+    hpo_network = _load_hpo_network(obo_file, terms_to_genes, annotations_count)
+
+    # score and output case hpo terms against all genes associated set of hpo terms
+    logger.info(f'Scoring case HPO terms from file: {case_hpo_file} against cases in: {records_file}')
+
+    # create instance the scorer class
+    scorer = Scorer(hpo_network)
+
+    try:
+        # read records_file
+        with open(records_file) as records_fh:
+            reader = csv.reader(records_fh, delimiter='\t')
+            records = {}
+            for line in reader:
+                if line[0].startswith('#'):
+                    continue
+                records[line[0]] = line[1].split('|')
+    except (FileNotFoundError, PermissionError) as e:
+        logger.critical(e)
+        exit(1)
+
+    # include the case-to-iteslf
+    records['case'] = case_hpo
+    # iterate over each cross-product and score the pair of records
+    manager = Manager()
+    lock = manager.Lock()
+    with Pool(threads) as p:
+        p.starmap(scorer.score_pairs, [(records, [('case', record) for record in records], lock, i, threads) for i in range(threads)])
+
+
+def score_product(records_file, obo_file=None, pheno2genes_file=None, threads=1):
     """
     Scores the cartesian product of HPO terms from a list of unique records (cases, genes, diseases, etc).
 
@@ -147,7 +214,8 @@ def score_all(records_file, obo_file=None, pheno2genes_file=None, threads=1):
 def main():
     fire.Fire({
         'score': score_case_to_genes,
-        'score-all': score_all,
+        'score-cases': score_case_to_cases,
+        'score-product': score_product,
     })
 
 
