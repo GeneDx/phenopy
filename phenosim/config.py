7c666008c29535256a50267ea02a12ee95bd95ec
Vlad Gainullin <vgainullin@genedx.com>
2019-08-09 17:43:23 -0400

    add download hpo files automatically if they do not exist

--- phenosim/config.py
+++ phenosim/config.py
@@ -6,6 +6,7 @@ import shutil
 
 from phenosim import __project__, __version__
 
+
 def download_hpo_files():
     """
     Check if HPO files exist, if not download them
@@ -18,23 +19,43 @@ def download_hpo_files():
         :param file_path: where to put it
         :return: None
         """
-        with urllib.request.urlopen(url) as response, open(file_path, 'wb') as out_file:
-            shutil.copyfileobj(response, out_file)
+        try:
+            with urllib.request.urlopen(url) as response, open(file_path, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+
+        except PermissionError:
+            logger.info('No permission accessing data directory: %s' % file_path)
+            exit(1)
+
+        except ValueError:
+            logger.info('Incorrect url specified for HPO files: %s' % url)
+            raise
+
+        except urllib.error.URLError:
+            logger.info('Incorrect url specified for HPO files: %s' % url)
+            raise
+
+        except Exception as unhandled_exception:
+            logger.info('Could not download HPO files')
+            logger.info('Try downloading them manually')
+            raise unhandled_exception
 
 
-    # read the config file to get file paths and urls
-    phen_to_genes_path = config.get('hpo','pheno2genes_file')
-    phen_to_genes_url = config.get('hpo','pheno2genes_file_url')
+        # read the config file to get file paths and urls
+        phen_to_genes_path = config.get('hpo','pheno2genes_file')
+        phen_to_genes_url = config.get('hpo','pheno2genes_file_url')
 
-    obo_path = config.get('hpo','obo_file')
-    obo_url = config.get('hpo','obo_file_url')
+        obo_path = config.get('hpo','obo_file')
+        obo_url = config.get('hpo','obo_file_url')
 
-    if not os.path.isfile(phen_to_genes_path):
-        logger.info('Downloading HPO phenotype to: %s'%phen_to_genes_path)
-        download(phen_to_genes_url, phen_to_genes_path)
-    if not os.path.isfile(obo_path):
-        logger.info('Downloading HPO obo file to: %s'%obo_path)
-        download(obo_url, obo_path)
+        if not os.path.isfile(phen_to_genes_path):
+            logger.info('Downloading HPO phenotype to: %s'%phen_to_genes_path)
+            download(phen_to_genes_url, phen_to_genes_path)
+
+        if not os.path.isfile(obo_path):
+            logger.info('Downloading HPO obo file to: %s'%obo_path)
+            download(obo_url, obo_path)
+
 
 
 # create logger
@@ -100,9 +121,6 @@ config.read(config_file)
 logger.info(f'Using configuration file: {config_file}')
 
 
-try:
-    logger.info('Checking if HPO files exist')
-    download_hpo_files()
-except FileNotFoundError:
-    logger.info('Error downloading hpo files')
-    logger.info('Try rerunning or download necessary files to: %s'%data_directory)
+logger.info('Checking if HPO files exist')
+download_hpo_files()
+
