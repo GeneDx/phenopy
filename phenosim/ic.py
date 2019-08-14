199c1c1ce1a4b8d0d15b2e095e2c20d6872dec91
Kevin Arvai <karvai@genedx.com>
2019-08-14 14:17:47 -0400

    handle ZeroDivisionError warnings in ic.py

--- phenosim/ic.py
+++ phenosim/ic.py
@@ -4,7 +4,7 @@ import numpy as np
 SMOOTH = 1
 
 
-def calculate_information_content(hpo_id, hpo_network, terms_to_genes, a_count, custom_annotations=None):
+def calculate_information_content(hpo_id, hpo_network, terms_to_genes, a_count, custom_annotations):
     """
     Calculates information content for an HPO term.
 
@@ -23,11 +23,17 @@ def calculate_information_content(hpo_id, hpo_network, terms_to_genes, a_count, 
         count_hpo_annos = sum([len(annotations[h]) for h in hpo_ids if h in annotations])
         # negative log of the number of hpo annotations divided by the total number of hpo terms in the
         # phenotypes_to_genes file
-        return -np.log((count_hpo_annos + SMOOTH) / float(a_count + SMOOTH))
+        information_content = -np.log((count_hpo_annos + SMOOTH) / float(a_count + SMOOTH))
+        # include this to avoid returning zero division warnings.
+        # when comparing two different leaves whose LCA is "phenotypic abnormality", the right side of the equation:
+        # alphaIC / (alphaIC / betaIC), would be 0.0 / (0.0 / 0.0)
+        if np.isinf(information_content) or np.isnan(information_content):
+            return np.nextafter(0, 1)
+        return information_content
 
     annotations_list = [terms_to_genes]
     if custom_annotations is not None:
-        annotations_list.extend(custom_annotations)
+        annotations_list.append(custom_annotations)
 
     return np.mean([get_ic(hpo_id_plus_children, annotations=annotations) for annotations in annotations_list])
 
