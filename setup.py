c0757b6fde2c8b32a4cb3c2a1b78d07212a4b6c1
Kevin Arvai <karvai@genedx.com>
2019-09-10 11:47:02 -0400

    version bump and include image in README

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from setuptools import find_packages, setup
 
 from phenopy import __project__, __version__
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name=__project__,
     packages=find_packages(),
     version=__version__,
     description='Phenotype comparison scoring by semantic similarity.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Kevin Arvai <karvai@genedx.com>, Kyle Retterer <retterer@genedx.com>, Carlos Borroto <cborroto@genedx.com>, Vlad Gainullin <vgainullin@genedx.com>',
     author_email='<datascience@genedx.com>',
     license='',
