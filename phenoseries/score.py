6eb2c29d838d6fa0515b605a29a436ad2d2ec7cc
Kevin Arvai <karvai@genedx.com>
2020-05-31 17:19:25 -0400

    add ranking script

---
+++ phenoseries/score.py
@@ -0,0 +1,4 @@
+# For now use this command
+# phenopy score phenoseries.mim_hpo_ids.txt --self --threads 48 >phenoseries.hrss.bma.pairwise.txt
+
+# still need to add Resnik, cosine, jaccard, w2v, d2v scoring methods
